0ed20b21e2141e81cdd9f159edf932f7f3c936be
diegor <diegorutti26@gmail.com>
2026-03-02 14:24:03 -0500

    nuevo

--- blog/detalle/detalle.js
+++ blog/detalle/detalle.js
@@ -482,6 +482,52 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // =======================
+// DRAG RELATED SLIDER
+// =======================
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("dragging");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("dragging");
+});
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("dragging");
+});
+
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.2; 
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+// TOUCH MOBILE
+
+slider.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].pageX;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("touchmove", (e) => {
+    const x = e.touches[0].pageX;
+    const walk = (x - startX) * 1.2;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
 });
 
 window.addEventListener("load", () => {
